0ec9d129a297275e1df2db42a36c066781bd4407
computboy <wel_sun@zju.edu.cn>
2026-05-28 15:43:07 +0800

    chore: initial SwiftUI project

---
+++ CoDesign-Agent/CoDesign_AgentApp.swift
@@ -0,0 +1,32 @@
+//
+//  CoDesign_AgentApp.swift
+//  CoDesign-Agent
+//
+//  Created by mac on 2026/5/28.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CoDesign_AgentApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CoDesign-AgentTests/CoDesign_AgentTests.swift
@@ -0,0 +1,18 @@
+//
+//  CoDesign_AgentTests.swift
+//  CoDesign-AgentTests
+//
+//  Created by mac on 2026/5/28.
+//
+
+import Testing
+
+struct CoDesign_AgentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
